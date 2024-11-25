9688e38f2f3f1a2356219df029574cf9de055301
Anh Dinh <anhdinh17@gmail.com>
2024-11-25 09:23:16 -0600

    Initial Commit

---
+++ AnhDinhTakeHomeExercise/AnhDinhTakeHomeExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnhDinhTakeHomeExerciseApp.swift
+//  AnhDinhTakeHomeExercise
+//
+//  Created by Anh Dinh on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AnhDinhTakeHomeExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnhDinhTakeHomeExercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnhDinhTakeHomeExercise
+//
+//  Created by Anh Dinh on 11/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
